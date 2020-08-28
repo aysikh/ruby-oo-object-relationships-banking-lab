b13d64a4caaf147d6c1cabe1eb6076815d35c246
Aymes <aysikh@Aymess-MBP.fios-router.home>
2020-08-28 15:46:35 -0400

    Finished whole lab

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_accessor :amount, :sender, :receiver, :status
+
+  def initialize(sender, receiver, amount)
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if self.valid? == true && sender.balance >= amount 
+      until self.status == "complete"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+      end
+      # binding.pry
+    else 
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    end
+  end
+
+
 end
